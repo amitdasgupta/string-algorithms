027510f3bf373d9d52a6ae8c67d65041ecb03084
Amit Das Gupta <amitdsgupta007@gmail.com>
2017-11-21 18:31:06 +0530

    rabin karp string matching algorithm.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include<string>
+#include<math.h>
+#define prime 101
 using namespace std;
-/************brute force algorithm for string matching*//////////////
+/************brute force algorithm for string matching
 int matchBrute(string a,string b)
 {
     int m=a.size()-1,n=b.size()-1,j,res;
@@ -17,10 +19,52 @@ int matchBrute(string a,string b)
             return i;
     }
 }
+*//////////////
+/**************create hash function*/////////////////////
+long hashOfString(string a,int start,int last)
+{
+    long has=0;
+    for(int i=start;i<=last;i++)
+    {
+
+        has+=long(a[i])*pow(prime,i-start);
+       // cout<<has<<endl;
+    }
+    return has;
+}
+int stringMatchingRabinKarp(string a,string b)
+{
+    long i=0,j,p,t,m=a.size(),n=b.size();
+    p=hashOfString(a,0,m-1);
+    t=hashOfString(b,0,m-1);
+   // cout<<p<<" "<<t;
+    for(i=0;i<=n-m;i++)
+    {
+        cout<<p<<" "<<t<<endl;
+        j=0;
+        if(p==t)
+        {
+           while(a[j]==b[i+j])
+           j++;
+           if(j==m)
+            return i;
+        }
+        else
+            if(i<n-m)
+        {
+            t=(t-long(b[i]))/prime;
+            t+=(long(b[i+m])*pow(prime,m-1));
+
+        }
+        else
+            return -1;
+    }
+}
+
 int main()
 {
     string a,b;
     cin>>a>>b;
-    cout<<endl<<matchBrute(a,b);
+    cout<<stringMatchingRabinKarp(a,b);
     return 0;
 }
